ba8b7b5978909195b6bba1cc409ee7707727c272
PhamGiaKhanh1912 <122188384+PhamGiaKhanh1912@users.noreply.github.com>
2023-11-28 20:25:35 +0700

    Add files via upload

---
+++ Finalweb-advance JSI-02/index.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    $('#row3').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+    });
+  });
+  $(document).ready(function(){
+    $('#row4').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+    });
+  });
+  $(document).ready(function(){
+    $('#row5').slick({
+  slidesToShow:4 ,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+    });
+  });
+
